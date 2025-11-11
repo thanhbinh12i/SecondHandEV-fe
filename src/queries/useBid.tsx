69a437d9b54649a925905ea0a0b6dd4e310d0dfe
thanhbinh12i <nptbinh17092004@gmail.com>
2025-11-11 16:29:32 +0700

    update: refetch bid and auction

--- src/queries/useBid.tsx
+++ src/queries/useBid.tsx
@@ -1,23 +1,35 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import bidApiRequests from "src/api/bid";
 
 export const usePostBidMutation = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: bidApiRequests.postBid,
+    onSuccess: (data, variables) => {
+      queryClient.invalidateQueries({ queryKey: ["bid", variables.auctionId] });
+    },
   });
 };
 
 export const useGetBidList = ({
   id,
   enabled,
+  refetchInterval,
+  refetchIntervalInBackground,
 }: {
   id: number;
   enabled: boolean;
+  refetchInterval?: number | false;
+  refetchIntervalInBackground?: boolean;
 }) => {
   return useQuery({
     queryKey: ["bid", id],
     queryFn: () => bidApiRequests.bidList(id),
     enabled,
+    refetchInterval: refetchInterval,
+    refetchIntervalInBackground: refetchIntervalInBackground ?? false,
+    staleTime: 0,
   });
 };
 
